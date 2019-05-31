489ee9f9b4cea2370f91ace914aa8c519f22d0bc
hyeongjun94 <whow1101@naver.com>
2019-06-01 01:18:31 +0900

    first commit

---
+++ web_ui/src/components/Content.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import '../App.css'
+
+class Content extends Component {
+    
+    render(){
+        return (
+            <div className="content">
+                content
+            </div>
+        )
+    }
+}
+
+
+
+export default Content
---
+++ web_ui/src/components/LogIn.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+// import {Button, Grid} from 'semantic-ui-react'
+// import 'semantic-ui-css/semantic.min.css';
+import '../App.css'
+
+class LogIn extends Component {
+    render(){
+        return (
+            <div className="login">
+                <button>Log In</button>
+                <button>Register</button>
+            </div>
+        )
+    }
+}
+
+
+
+export default LogIn
