4de7e6cf26d8689b3d8ad1d957777562aa0a3f9a
hyeongjun94 <whow1101@naver.com>
2019-07-29 19:47:03 +0900

    create release branch

--- web_ui/src/index.js
+++ web_ui/src/index.js
@@ -15,6 +15,7 @@ const store = createStore(rootReducer, composeWithDevTools());
 const theme = createMuiTheme({
   spacing: factor => [0, 4, 8, 6][factor],
 });
+// Release Version
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
